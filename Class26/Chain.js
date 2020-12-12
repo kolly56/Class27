7dd0be80e2431540af4d4678a478b8174487c4f0
kolly56 <73864619+kolly56@users.noreply.github.com>
2020-12-11 18:40:22 -0800

    Add files via upload

---
+++ Class26/Chain.js
@@ -0,0 +1,19 @@
+class Chain{
+    constructor(body1,body2){
+        var options={
+            bodyA: body1,
+            bodyB: body2, 
+            stiffness:0.04,
+            length:10  
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+    
+    }
+    display(){
+      var  point1=this.chain.bodyA.position;
+      var  point2=this.chain.bodyB.position;
+        strokeWeight(4);
+        line(point1.x,point1.y,point2.x,point2.y);
+    }
+}
